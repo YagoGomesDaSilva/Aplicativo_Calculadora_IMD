b0dad72b9a2dfcda2d77d6eee244e943f8644a38
Yago Gomes Da Silva <99800161+YagoGomesDaSilva@users.noreply.github.com>
2023-09-30 15:27:30 -0300

    Commit inicial de criação

---
+++ app/src/main/java/br/com/god/aplicativo_calculadora_imd/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.god.aplicativo_calculadora_imd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
