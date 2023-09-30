4d31edc40ca4e8970f760ec5460154b2bfa3a59f
Yago Gomes Da Silva <99800161+YagoGomesDaSilva@users.noreply.github.com>
2023-09-30 18:42:46 -0300

    Limpeza de codigo

--- app/src/main/java/br/com/god/aplicativo_calculadora_imd/FragmentCalNum.java
+++ app/src/main/java/br/com/god/aplicativo_calculadora_imd/FragmentCalNum.java
@@ -8,11 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentCalNum#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentCalNum extends Fragment {
 
     public FragmentCalNum() { }
