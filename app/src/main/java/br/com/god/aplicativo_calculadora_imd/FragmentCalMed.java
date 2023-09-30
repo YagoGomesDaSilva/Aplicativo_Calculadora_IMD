7be8a5fa9a173efe4c706a3afe7a2ccbd8f629f1
Yago Gomes Da Silva <99800161+YagoGomesDaSilva@users.noreply.github.com>
2023-09-30 18:42:46 -0300

    Limpesa de codigo

--- app/src/main/java/br/com/god/aplicativo_calculadora_imd/FragmentCalMed.java
+++ app/src/main/java/br/com/god/aplicativo_calculadora_imd/FragmentCalMed.java
@@ -8,11 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentCalMed#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentCalMed extends Fragment {
 
     public FragmentCalMed() { }
