8c636c8090731284ef2f6c79e0208ea9d6f7b52e
Yago Gomes Da Silva <99800161+YagoGomesDaSilva@users.noreply.github.com>
2023-09-30 18:40:39 -0300

    Criação dos fragmentos

---
+++ app/src/main/java/br/com/god/aplicativo_calculadora_imd/FragmentCalNum.java
@@ -0,0 +1,29 @@
+package br.com.god.aplicativo_calculadora_imd;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentCalNum#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentCalNum extends Fragment {
+
+    public FragmentCalNum() { }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cal_num, container, false);
+    }
+}
