2588e960b24cd45182926f2cd2510587b3bb26d5
Yago Gomes Da Silva <99800161+YagoGomesDaSilva@users.noreply.github.com>
2023-10-01 14:04:25 -0300

    Implementação dos alert dialog e dos toast

--- app/src/main/java/br/com/god/aplicativo_calculadora_imd/MainActivity.java
+++ app/src/main/java/br/com/god/aplicativo_calculadora_imd/MainActivity.java
@@ -1,11 +1,13 @@
 package br.com.god.aplicativo_calculadora_imd;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,35 +22,37 @@ public class MainActivity extends AppCompatActivity {
         this.fragCalNum = new FragmentCalNum();
 
         this.swapFragment(R.id.mainFrameFrag,fragCalNum);
-//
-//        Button btn_numerica = this.findViewById(R.id.btn_numerica);
-//        Button btn_media = this.findViewById(R.id.btn_media);
+        this.createAlertDialog("Bem Vindo ao App!","Obigado!");
+
+
     }
 
-    public void btnFragmentMangager(View view){
-
-        int viewId = view.getId();
-        if(viewId == R.id.btn_numerica)
-            this.swapFragment(R.id.mainFrameFrag,fragCalNum);
-        if(viewId == R.id.btn_media)
-            this.swapFragment(R.id.mainFrameFrag,fragCalMed);
-/*
-        switch (view.getId()){
-            case R.id.btn_numerica:
-                this.swapFragment(R.id.mainFrameFrag,fragCalNum);
-                break;
-            case R.id.btn_media:
-                this.swapFragment(R.id.mainFrameFrag,fragCalMed);
-                break;
-        }
-        */
-
+    void createAlertDialog(String mainMessage, String btnMessage){
+        AlertDialog.Builder ad = new AlertDialog.Builder(this);
+        ad.setCancelable(false);
+        ad.setMessage(mainMessage);
+        ad.setNeutralButton(btnMessage, null);
+        ad.show();
     }
 
     private void swapFragment(int idFrame, Fragment frag){
         FragmentTransaction frag_tran = this.getSupportFragmentManager().beginTransaction();
         frag_tran.replace(idFrame,frag);
         frag_tran.commit();
+
     }
 
+    public void btnFragmentManagemant(View view){
+
+        int viewId = view.getId();
+        if(viewId == R.id.btn_numerica){
+            this.swapFragment(R.id.mainFrameFrag,fragCalNum);
+            Toast.makeText(this, "Função Calculadora Numerica", Toast.LENGTH_SHORT).show();
+        }
+
+        if(viewId == R.id.btn_media){
+            this.swapFragment(R.id.mainFrameFrag,fragCalMed);
+            Toast.makeText(this, "Função Calculadora de Media", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
