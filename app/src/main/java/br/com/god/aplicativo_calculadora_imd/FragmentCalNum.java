cbbd9b7ac76f8b690e96ffd1211a0f51d5db6386
Yago Gomes Da Silva <99800161+YagoGomesDaSilva@users.noreply.github.com>
2023-10-03 13:24:32 -0300

    Implementação do botão de limpeza e dos botões de operação

--- app/src/main/java/br/com/god/aplicativo_calculadora_imd/FragmentCalNum.java
+++ app/src/main/java/br/com/god/aplicativo_calculadora_imd/FragmentCalNum.java
@@ -9,8 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class FragmentCalNum extends Fragment {
+
+
+    String input_buffer;
+    String operator_buffer;
+    boolean opetator_select = false;
 
     public FragmentCalNum() { }
     @Override
@@ -21,54 +27,40 @@ public class FragmentCalNum extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cal_num, container, false);
-        EditText ed_input = view.findViewById(R.id.ed_input);
 
+        EditText ed_input = view.findViewById(R.id.ed_input);
+        TextView tv_result = view.findViewById(R.id.tv_result);
+        Button btn_clear = view.findViewById(R.id.btn_clear);
+
+
+        this.clearClickListener(btn_clear, ed_input,tv_result);
         this.NumberDotClickListener(view,ed_input);
+        this.operatorClickListener(view);
 
         return view;
     }
 
-    public void operatorClickListener(View view, EditText ed_input) {
-
-        Button btn_adicao = view.findViewById(R.id.btn_adicao);
-        Button btn_subtracao = view.findViewById(R.id.btn_subtracao);
-        Button btn_multiplicacao = view.findViewById(R.id.btn_multiplicacao);
-        Button btn_divisao = view.findViewById(R.id.btn_divisao);
-
-        btn_adicao.setOnClickListener(new View.OnClickListener() {
+    public void clearClickListener(Button button,EditText ed_input, TextView tv_result){
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                clear(ed_input, tv_result);
             }
         });
-
-        btn_subtracao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        btn_multiplicacao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        btn_divisao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                calcular(view);
-
-            }
-        });
-
     }
 
-    private void calcular(View view){
+    public void operatorClickListener(View view) {
 
+        for (int i = 1; i <= 4; i++) {
+            int buttonId = getResources().getIdentifier("btn_op_" + i, "id", getActivity().getPackageName());
+            Button button = view.findViewById(buttonId);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    setOperator(button);
+                }
+            });
+        }
     }
 
     public void NumberDotClickListener(View view, EditText ed_input) {
@@ -83,6 +75,22 @@ public class FragmentCalNum extends Fragment {
                 }
             });
         }
+    }
+
+    private void setOperator(Button button){
+        if(!opetator_select){
+            operator_buffer = button.getText().toString();
+            opetator_select = true;
+            //ed_input.setText(ed_input.getText() + button.getText().toString());
+        }
+    }
+
+    private void clear(EditText ed_input, TextView tv_result){
+        ed_input.setText("");
+        tv_result.setText("");
+        this.input_buffer = "";
+        this.operator_buffer = "";
+        this.opetator_select = false;
     }
 
 }
